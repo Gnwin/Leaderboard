c8a5d6c9b74ca9bc71e6024cc28497aab1844548
Godwin Nwachukwu <nwgwin@gmail.com>
2022-04-19 18:59:09 +0100

    edit readme and correct linter errors

--- src/assets/js/modules/App.js
+++ src/assets/js/modules/App.js
@@ -4,33 +4,32 @@ import key from './key.js';
 import activateButtons from './ActivateButtons.js';
 import fetchAndDisplaydata from './fetchAndDisplaydata.js';
 
-let gamename = 'My cool new game';
-let game = new Game(gamename);
+const gamename = 'My cool new game';
+const game = new Game(gamename);
 
-function runApp() {
+const runApp = () => {
+  function createID() {
+    return fetch(api, {
+      method: 'POST',
+      body: JSON.stringify(game),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        localStorage.setItem(key, JSON.stringify({ name: gamename }));
+        const leaderBoard = JSON.parse(localStorage.getItem(key));
+        const splittedResult = json.result.split(' ');
+        const [id] = [splittedResult[3]];
+        leaderBoard.id = id;
+        localStorage.setItem(key, JSON.stringify(leaderBoard));
+        const scores = document.querySelector('.scores');
+        scores.setAttribute('id', id);
+        return id;
+      });
+  }
   if (localStorage.getItem(key) === null) {
-    function createID() {
-      return fetch(api, {
-        method: 'POST',
-        body: JSON.stringify(game),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          localStorage.setItem(key, JSON.stringify({name: gamename}));
-          const leaderBoard = JSON.parse(localStorage.getItem(key));
-          const splittedResult = json.result.split(' ');
-          const [id] = [splittedResult[3]];
-          leaderBoard['id'] = id;
-          localStorage.setItem(key, JSON.stringify(leaderBoard));
-          const scores = document.querySelector('.scores');
-          scores.setAttribute('id', id);
-          return id;
-        });
-    }
-
     createID().then((uniqueid) => {
       fetchAndDisplaydata(uniqueid);
       activateButtons(uniqueid);
@@ -41,6 +40,5 @@ function runApp() {
     activateButtons(leaderBoard.id);
   }
 }
-
 
 export default runApp;
